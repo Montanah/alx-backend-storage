8105ec708992c0bbe971e33b80c66b91fb50387b
Montanah <mititi.isaac@gmail.com>
2023-08-10 15:58:26 -0700

    Trigger

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -7,7 +7,7 @@ CREATE TRIGGER update_quantity AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
     UPDATE items
-    SET quantity = quantity - NEW.number
+    SET quantity = quantity - 1
     WHERE id = NEW.item_id;
 END$$
 
