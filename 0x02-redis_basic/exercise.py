c38388526c5595f97c481e27b648babd3aa59b13
Montanah <mititi.isaac@gmail.com>
2023-08-16 20:53:47 +0000

    Writing strings to Redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -17,5 +17,5 @@ class Cache:
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Method that generates a random key"""
         key = str(uuid4())
-        self._redis.mset({key: data})
+        self._redis.set(key, data)
         return key
