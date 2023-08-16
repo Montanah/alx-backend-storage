4e540185a93947eee0e5092f0576f08718da6c3b
Montanah <mititi.isaac@gmail.com>
2023-08-16 20:41:00 +0000

    Writing strings to Redis

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Creating a Cache class."""
+
+import redis
+import uuid
+from typing import Union
+
+
+class Cache:
+    """Cache class"""
+
+    def __init__(self):
+        """Method constructor"""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """Method that generates a random key"""
+        key = str(uuid4())
+        self._redis.mset({key: data})
+        return key
